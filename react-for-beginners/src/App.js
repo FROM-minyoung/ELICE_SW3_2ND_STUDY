dae92321f665bed07d9b9695f5861c1164185ae5
minyoung <918liter@gmail.com>
2022-11-23 20:05:05 +0900

    7.0 강의 완료/Todo list 만들기

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,41 +1,37 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  /* create 될 때는 return 전 함수, destroyed 될 때는 return 후 함수! 함수다 함수가 와야해! 
-
-    useEffect(() => {
-    console.log("create :) ");
-    return () => console.log("destroyed X(");
-  }, []);
-
-  */
-
-  // 이런식으로 익명함수 말고 미리 만들어둔 함수를 적을 때는
-  // destroyed 될 때 그 함수 안에 return된 값이 나타난다
-
-  function byFn() {
-    console.log("bye :)");
-  }
-  function hiFn() {
-    console.log("hi :)");
-    return byFn; // destroyed 될 때 나타날 함수
-  }
-
-  useEffect(hiFn, []);
-
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  /* cleanup */
-  const [showing, setShowing] = useState(false);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
 
-  const onClick = () => setShowing((current) => !current);
-
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    // toDo가 빈칸이면 submit이 되지 않게하고, 빈칸이 아니면 submit 후
+    // input 칸 비워지게 하기
+    if (toDo === "") {
+      return;
+    }
+    // 주의! 객체인 경우는 이렇게!!! 기존값을 포함한 새 객체를 만들어 리턴해야함.
+    /* setToDos((currentArray) => {
+      const newArray = [toDo, ...currentArray];
+      return newArray;
+      }); */
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "HIDE" : "SHOW"}</button>
+      <h1>내 할 일 ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="작성해주세요."
+        />
+        <button>To do 추가하기</button>
+      </form>
     </div>
   );
 }
