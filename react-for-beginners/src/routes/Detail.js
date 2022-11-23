163711d71f21b6406b985da37e2c68358d85b5b7
minyoung <918liter@gmail.com>
2022-11-24 03:57:43 +0900

    7.4 강의 완료/router, components로 구분

---
+++ react-for-beginners/src/routes/Detail.js
@@ -0,0 +1,5 @@
+const Detail = () => {
+  retrun(<h1>Detail</h1>);
+};
+
+export default Detail;
---
+++ react-for-beginners/src/routes/Home.js
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import Movie from "../components/Movie";
+
+const Home = () => {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  /*
+  fetch then,catch 쓸 때
+useEffect(() => {
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year"
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  }, []);
+*/
+
+  /*
+  async await 쓸 때
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year"
+    );
+
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+*/
+
+  // 그걸 더 간결하게 쓸 때
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=9.0&sort_by=year"
+      )
+    ).json();
+
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Home;
