35cad51e9ec26f369d8b7008654908acb6121723
minyoung <918liter@gmail.com>
2022-11-28 02:14:04 +0900

    css: Header 컴포넌트 기본 배열 설정

--- react-for-beginners/src/components/Header.js
+++ react-for-beginners/src/components/Header.js
@@ -3,7 +3,10 @@ import "./Header.css";
 const Header = () => {
   return (
     <div>
-      <div className="page__title">MOVIE COLLECTION</div>
+      <div className="page__title">
+        MOVIE <br />
+        COLLECTION
+      </div>
     </div>
   );
 };
