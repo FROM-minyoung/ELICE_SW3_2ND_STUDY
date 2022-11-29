10cc47f80c21aaee2e5af6c25cb513b50f3ed510
minyoung <918liter@gmail.com>
2022-11-30 00:49:25 +0900

    update: header 효과 변경

--- react-for-beginners/src/components/Header.js
+++ react-for-beginners/src/components/Header.js
@@ -3,7 +3,7 @@ import "animate.css";
 
 const Header = () => {
   return (
-    <div className="page__title animate__animated animate__fadeInUp">
+    <div className="page__title animate__animated animate__fadeIn">
       MOVIE <br />
       COLLECTION
     </div>
